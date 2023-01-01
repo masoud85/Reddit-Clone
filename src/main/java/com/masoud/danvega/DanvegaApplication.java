5850d59a2c9041e880de0a231b127406bee7e7d2
Masoud <masoud.comp12@gmail.com>
2023-01-01 21:48:30 +0100

    Project setup.

---
+++ src/main/java/com/masoud/danvega/DanvegaApplication.java
@@ -0,0 +1,13 @@
+package com.masoud.danvega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanvegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanvegaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/masoud/danvega/DanvegaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masoud.danvega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanvegaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
